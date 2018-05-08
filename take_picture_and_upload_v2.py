386ae8137b6e931d206fa6d6b9a3aa72a31e5fe5
Jake Johnson <jjohnson5253@yahoo.com>
2018-05-08 04:54:52 +0000

    takes lower res pic but only works once for some reason

--- take_picture_and_upload_v2.py
+++ take_picture_and_upload_v2.py
@@ -1,9 +1,13 @@
 from picamera import PiCamera
+from PIL import Image
 import os
 
 camera = PiCamera()
 
-camera.capture('gotcha.jpg')
+camera.capture('gotcha_orig.jpg')
+
+im = Image.open("gotcha_orig.jpg")
+im.save("gotcha.jpg", dpi=(300,300))
 
 os.system('scp gotcha.jpg jjohnson5253@skillpouch.com:/home/jjohnson5253/public_html/img/')
 
